97b1addc4fdc0bf91dd6ddfa0b88c524aa571734
GOLLA ASREETHA SREEJA <155885374+ASREETHASREEJA5@users.noreply.github.com>
2026-02-08 11:23:23 +0530

    Refactor imports and improve model loading

--- app_nli.py
+++ app_nli.py
@@ -5,7 +5,7 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_groq import ChatGroq
-from langchain.chains import RetrievalQA
+from langchain.chains.retrieval_qa.base import RetrievalQA
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 import tempfile
 import os
@@ -312,5 +312,6 @@ if uploaded_file:
         except PermissionError:
 
             st.warning("Temporary file is still in use and cannot be removed.")
+
 
 
