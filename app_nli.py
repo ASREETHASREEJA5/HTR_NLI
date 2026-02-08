8001dd5efc3c9c20bd3f476055c4bd66d7620761
GOLLA ASREETHA SREEJA <155885374+ASREETHASREEJA5@users.noreply.github.com>
2026-02-08 11:33:16 +0530

    Refactor app_nli.py for improved structure and readability

--- app_nli.py
+++ app_nli.py
@@ -5,7 +5,7 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_groq import ChatGroq
-from langchain.chains.retrieval_qa.base import RetrievalQA
+
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 import tempfile
 import os
@@ -312,6 +312,7 @@ if uploaded_file:
         except PermissionError:
 
             st.warning("Temporary file is still in use and cannot be removed.")
+
 
 
 
